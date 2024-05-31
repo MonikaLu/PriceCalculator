7efaa7ffa23ac75996fa0b5012eac1794151efb4
Monika Luu <monika-luu@hotmail.com>
2024-05-31 21:57:11 +0200

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { TextField } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Price Calculator!</h1>
+      <TextField id="outlined-basic" label="Product" variant="outlined" />
+    </>
+  );
+}
+
+export default App;
